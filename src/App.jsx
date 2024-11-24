f2bb6920cd68b5f3d268dd70d9fad10fd41b69ae
Mahdi Jalalifar <Mjalalimech@gmail.com>
2024-11-24 17:51:13 +0400

    install react-router-dom and build main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import HomeProperties from './pages/HomeProperties'
 
 function App() {
   return (
-    <div className='text-blue-500'>
-      App
+    <div>
+      <Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/homeproperties' element={<HomeProperties/>} />
+      </Routes>
     </div>
   )
 }
